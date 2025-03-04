231f35a607b4396a191389f6737ed4c5c44877ff
boki <lsb530@naver.com>
2025-03-04 20:23:30 +0900

    init

---
+++ src/main/kotlin/com/boki/kotlinthymeleafportfolio/KotlinThymeleafPortfolioApplication.kt
@@ -0,0 +1,11 @@
+package com.boki.kotlinthymeleafportfolio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinThymeleafPortfolioApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinThymeleafPortfolioApplication>(*args)
+}
---
+++ src/test/kotlin/com/boki/kotlinthymeleafportfolio/KotlinThymeleafPortfolioApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.boki.kotlinthymeleafportfolio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinThymeleafPortfolioApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
