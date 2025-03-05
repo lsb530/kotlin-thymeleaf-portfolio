35f5cd0410ce389fcbe80855e9d9d8162a5bd7f2
boki <lsb530@naver.com>
2025-03-05 23:03:34 +0900

    서비스 개발

--- src/main/kotlin/com/boki/kotlinthymeleafportfolio/presentation/service/PresentationService.kt
+++ src/main/kotlin/com/boki/kotlinthymeleafportfolio/presentation/service/PresentationService.kt
@@ -1,8 +1,51 @@
 package com.boki.kotlinthymeleafportfolio.presentation.service
 
+import com.boki.kotlinthymeleafportfolio.presentation.dto.IntroductionDTO
+import com.boki.kotlinthymeleafportfolio.presentation.dto.LinkDTO
+import com.boki.kotlinthymeleafportfolio.presentation.dto.ProjectDTO
+import com.boki.kotlinthymeleafportfolio.presentation.dto.ResumeDTO
+import com.boki.kotlinthymeleafportfolio.presentation.repository.PresentationRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
-class PresentationService {
+class PresentationService(
+    private val presentationRepository: PresentationRepository
+) {
+
+    @Transactional(readOnly = true)
+    fun getIntroductions(): List<IntroductionDTO> {
+        val introductions = presentationRepository.getActiveIntroductions()
+
+        return introductions.map { IntroductionDTO(it) }
+    }
+
+    @Transactional(readOnly = true)
+    fun getLinks(): List<LinkDTO> {
+        val links = presentationRepository.getActiveLinks()
+
+        return links.map { LinkDTO(it) }
+    }
+
+    @Transactional(readOnly = true)
+    fun getResume(): ResumeDTO {
+
+        val experiences = presentationRepository.getActiveExperiences()
+        val achievements = presentationRepository.getActiveAchievements()
+        val skills = presentationRepository.getActiveSkills()
+
+        return ResumeDTO(
+            experiences = experiences,
+            achievements = achievements,
+            skills = skills
+        )
+    }
+
+    @Transactional(readOnly = true)
+    fun getProjects(): List<ProjectDTO> {
+        val projects = presentationRepository.getActiveProjects()
+
+        return projects.map { ProjectDTO(it) }
+    }
 
 }
