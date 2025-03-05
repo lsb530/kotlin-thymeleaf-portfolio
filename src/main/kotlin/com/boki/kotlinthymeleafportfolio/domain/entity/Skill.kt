56abed359459026c059bdb2468814224ca47fe84
boki <lsb530@naver.com>
2025-03-05 10:50:43 +0900

    엔티티 개발 - 연관관계 없음

--- src/main/kotlin/com/boki/kotlinthymeleafportfolio/domain/entity/Skill.kt
+++ src/main/kotlin/com/boki/kotlinthymeleafportfolio/domain/entity/Skill.kt
@@ -1,17 +1,29 @@
 package com.boki.kotlinthymeleafportfolio.domain.entity
 
+import com.boki.kotlinthymeleafportfolio.domain.constant.SkillType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 
 @Entity
-class Skill : BaseEntity() {
+class Skill(name: String, type: String, isActive: Boolean) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "skill_id")
     var id: Long? = null
 
+    var name: String = name
+
+    @Column(name = "skill_type")
+    @Enumerated(value = EnumType.STRING)
+    var type: SkillType = SkillType.valueOf(type)
+
+    var isActive: Boolean = isActive
+
 }
+
