60fc2192a6cdf50c5244fb2b57dcf15889c213fc
boki <lsb530@naver.com>
2025-03-06 14:23:10 +0900

    인터셉터 개발

--- src/main/kotlin/com/boki/kotlinthymeleafportfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/boki/kotlinthymeleafportfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,8 +1,21 @@
 package com.boki.kotlinthymeleafportfolio.presentation.interceptor
 
+import com.boki.kotlinthymeleafportfolio.domain.entity.HttpInterface
+import com.boki.kotlinthymeleafportfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
 
 @Component
-class PresentationInterceptor {
+class PresentationInterceptor(
+    private val httpInterfaceRepository: HttpInterfaceRepository
+) : HandlerInterceptor {
+
+    override fun afterCompletion(request: HttpServletRequest, response: HttpServletResponse, handler: Any, ex: Exception?) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
 
 }
+
